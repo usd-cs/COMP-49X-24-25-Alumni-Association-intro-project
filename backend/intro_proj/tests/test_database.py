c826f6d959beedaab7305631eb8c0a870d84444e
Connor Boll <cboll@sandiego.edu>
2024-11-11 11:06:58 -0800

    Finished up creating all database tests. Passing all tests. Merge request incoming

--- backend/intro_proj/tests/test_database.py
+++ backend/intro_proj/tests/test_database.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from intro_proj.models import User
+from intro_proj.models import Post
+from intro_proj.models import Comment
 
 # Create your tests here.
 
@@ -10,8 +12,79 @@ class DatabaseTests(TestCase):
     def tearDown(self):
         pass
 
-    def test_creation(self):
+    def test_user_creation(self):
         test_user = User.objects.create(email='user@sandiego.edu', name='John User')
         user = User.objects.get(email='user@sandiego.edu')
         self.assertEqual('user@sandiego.edu', user.email)
+
+    def test_post_creation(self):
+        test_user = User.objects.create(email='user@sandiego.edu', name='John User')
+        test_post = Post.objects.create(author=test_user, content='cool post')
+        post = Post.objects.get(post_ID=test_post.post_ID)
+        self.assertEqual('cool post', post.content)
     
+    def test_comment_creation(self):
+        test_user = User.objects.create(email='user@sandiego.edu', name='John User')
+        test_post = Post.objects.create(author=test_user, content='cool post')
+        test_comment = Comment.objects.create(author=test_user, parent_post=test_post, content='cool comment')
+        comment = Comment.objects.get(comment_ID=test_comment.comment_ID)
+        self.assertEqual('cool comment', comment.content)
+    
+    def test_post_cascade(self):
+        test_user = User.objects.create(email='user@sandiego.edu', name='John User')
+        test_post = Post.objects.create(author=test_user, content='cool post')
+        post_id = test_post.post_ID
+        test_user.delete()
+        post_exists = Post.objects.filter(post_ID=post_id).exists()
+        self.assertFalse(post_exists)
+    
+    def test_comment_post_cascade(self):
+        test_user = User.objects.create(email='user@sandiego.edu', name='John User')
+        test_post = Post.objects.create(author=test_user, content='cool post')
+        test_comment = Comment.objects.create(author=test_user, parent_post=test_post, content='cool comment')
+        comment_id = test_comment.comment_ID
+        test_post.delete()
+        comment_exists = Comment.objects.filter(comment_ID=comment_id).exists()
+        self.assertFalse(comment_exists)
+    
+    def test_comment_user_cascade(self):
+        test_user = User.objects.create(email='user@sandiego.edu', name='John User')
+        test_user_2 = User.objects.create(email='user2@sandiego.edu', name='John User2')
+        test_post = Post.objects.create(author=test_user, content='cool post')
+        test_comment = Comment.objects.create(author=test_user_2, parent_post=test_post, content='cool comment')
+        comment_id = test_comment.comment_ID
+        test_user_2.delete()
+        comment_exists = Comment.objects.filter(comment_ID=comment_id).exists()
+        self.assertFalse(comment_exists)
+
+    def test_user_duplicate_email(self):
+        User.objects.create(email='user@sandiego.edu', name='John User')
+        with self.assertRaises(Exception):
+            User.objects.create(email='user@sandiego.edu', name='Another User')
+
+    def test_post_timestamp(self):
+        test_user = User.objects.create(email='user@sandiego.edu', name='John User')
+        test_post = Post.objects.create(author=test_user, content='cool post')
+        self.assertIsNotNone(test_post.time_posted)
+
+    def test_comment_timestamp(self):
+        test_user = User.objects.create(email='user@sandiego.edu', name='John User')
+        test_post = Post.objects.create(author=test_user, content='cool post')
+        test_comment = Comment.objects.create(author=test_user, parent_post=test_post, content='cool comment')
+        self.assertIsNotNone(test_comment.time_posted)
+
+    def test_multiple_comments_on_post(self):
+        test_user = User.objects.create(email='user@sandiego.edu', name='John User')
+        test_post = Post.objects.create(author=test_user, content='cool post')
+        Comment.objects.create(author=test_user, parent_post=test_post, content='cool 1')
+        Comment.objects.create(author=test_user, parent_post=test_post, content='cool 2')
+        self.assertEqual(Comment.objects.filter(parent_post=test_post).count(), 2)
+
+    def test_multi_comment_cascade(self):
+        test_user = User.objects.create(email='user@sandiego.edu', name='John User')
+        test_post = Post.objects.create(author=test_user, content='cool post')
+        Comment.objects.create(author=test_user, parent_post=test_post, content='cool 1')
+        Comment.objects.create(author=test_user, parent_post=test_post, content='cool 2')
+        test_post_id = test_post.post_ID
+        test_post.delete()
+        self.assertEqual(Comment.objects.filter(parent_post=test_post_id).count(), 0)
